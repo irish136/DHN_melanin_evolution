0ff6a8dd14b27daabf19a8eef513c56c457757d4
irish136 <62709970+irish136@users.noreply.github.com>
2022-05-07 09:35:40 +0200

    Update and rename 6_hmmsearch_intersection_genes_TO_DO.py to 6_hmmsearch_intersection_genes.py

--- s8_make_species_tree/6_hmmsearch_intersection_genes.py
+++ s8_make_species_tree/6_hmmsearch_intersection_genes.py
@@ -1,13 +1,20 @@
+# This code saves all genes that are present only once in each species, but
+# at the same time present in all species
+# Input: HMMsearch output
+# Output 1: .txt file containing all genes that are present once in each species
+# Output 2: .txt file containing all genes that are present once in each species and present in X/all species
+
+# Import packages
 import gzip
 import os
 
-# Input: one gff file
-# Output: adjusted gff file
-
+# Get relevant directories
 input_path = 'output_hmmsearch/'
 output_path = 'hmmsearch_intersection/'
 result = os.listdir(input_path)
 
+# Variables
+number_of_species = 938
 
 def get_intersections(file):
     genes_present_once_in_species = []
@@ -18,7 +25,7 @@ def get_intersections(file):
         genes_in_species = []
 
         for line in f:
-            if not line.startswith('#'):
+            if not line.startswith('#'): # skip commented lines
                 gene_name = line.split('-')[1]
                 genes_in_species.append(gene_name.strip())
 
@@ -28,20 +35,17 @@ def get_intersections(file):
                 genes_present_once_in_species.append(gene)
     return genes_present_once_in_species
 
-# Save filenames from hmmsearch output in files
+# Get filenames from hmmsearch output
 files = []
 for i in result:
-    if i != 'predicted_genes_aa_Aspoch1.txt.gz' and i != 'predicted_genes_aa_Aspnom1.txt.gz' and i != 'predicted_genes_aa_Xylcur1.txt.gz' and i != 'predicted_genes_aa_Aspcal1.txt.gz':
+    # Four files need to be excluded
+    if i != 'predicted_genes_aa_Aspoch1.txt.gz' and i != 'predicted_genes_aa_Aspnom1.txt.gz' and \
+            i != 'predicted_genes_aa_Xylcur1.txt.gz' and i != 'predicted_genes_aa_Aspcal1.txt.gz':
         files.append(i)
-
-# Use a counter to keep track of progress
-counter = 0
 
 all_presence_once = []
 
 for file in files:
-    print('file loop: ', counter)
-    counter += 1
     # Save list with genes that are present once in one hmmsearch output file
     present_once_in_species = get_intersections(file)
 
@@ -52,18 +56,18 @@ for file in files:
 # Check which genes are present in all species (but inside one species just once)
 intersection_result = []
 
-present_genes_list = list(set(all_presence_once))
+present_genes_list = list(set(all_presence_once)) # Remove duplicates
 
 for gene in present_genes_list:
-    if all_presence_once.count(gene) >= 900: #should be 938
+    if all_presence_once.count(gene) >= number_of_species:
         intersection_result.append(gene)
 
 # Save intersection result
-intersection_file = open(output_path + 'intersection_file_3.txt', 'w')
+intersection_file = open(output_path + 'intersection_file.txt', 'w')
 for gene in intersection_result:
     intersection_file.write(gene+ '\n')
 
 # Save list with genes that occur once in each species, but from all species in one file
-test_output_file = open(output_path + 'all_presence_once_good.txt', 'w')
+output_file = open(output_path + 'all_presence_once_good.txt', 'w')
 for gene in all_presence_once:
-    test_output_file.write(gene+ '\n')
+    output_file.write(gene+ '\n')
