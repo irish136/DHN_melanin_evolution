95c1ed20308805e453b0b812c14d6e3f063c5b14
irish136 <62709970+irish136@users.noreply.github.com>
2022-05-07 09:01:22 +0200

    Update and rename 1_move_gff_and_assembly_file_TO_DO.py to 1_move_gff_and_assembly_file.py

--- s8_make_species_tree/1_move_gff_and_assembly_file.py
+++ s8_make_species_tree/1_move_gff_and_assembly_file.py
@@ -1,16 +1,24 @@
-import os
+# The downloaded JGI files are structured according to the taxonomy
+# This code puts all species GFF and fasta files into two separate folders
+# Input are the downloaded JGI files in taxonomy order
+# Output are two folders containing the GFF and fasta files
 
-# abspath to a folder as a string
+# Import packages
+import os
 import shutil
 
+# Define the new folders
+new_assembly_path = '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/assemblies'
+new_gff_path = '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/gffs'
+
+# Get current working directory
 dir = os.getcwd()
 
 folders_to_remove = []
 
-new_assembly_path = '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/assemblies'
-new_gff_path = '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/gffs'
-
+# Loop over downloaded JGI data
 for root, dirs, files in os.walk(dir):
+    # The new GFF and fasta folder need to be skipped
     if root != '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/gffs' and root != '/home/i.huitink_cbs-niob.local/h/data/copy_jgi/classes/assemblies':
         for file in files:
             if file.endswith('gff.gz') or file.endswith('gff3.gz'):
