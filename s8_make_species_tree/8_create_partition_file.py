f53472ec0d5e00b78a73c2bfd72d92c4d51fbcb7
irish136 <62709970+irish136@users.noreply.github.com>
2022-05-09 09:44:12 +0200

    Update 8_create_partition_file.py

--- s8_make_species_tree/8_create_partition_file.py
+++ s8_make_species_tree/8_create_partition_file.py
@@ -1,38 +1,46 @@
+# This code creates the partition file which can be used by IQtree to build a tree based on multiple trees
+# Input: .txt file with species names, trimmed fasta files per gene
+# Output: new fasta file with all genes and all species + partition file
+
+# Import packages
 import os
 import gzip
 
-#IQtree_files_path = 'output_iqtree_v3/'
+# Open directories
 trimal_files_path = 'output_trimal_per_gene_v3/'
 output_path = 'output_partition_files_v3/'
+IQtree_files_path = 'output_iqtree_v3/'
 
 new_fasta = open(output_path + 'new_fasta.fasta', 'w+')
 
 species_file = open('840_species_names.txt').readlines()
 
-# partition_file = open(output_path + 'partition_file.nex', 'w+')
-# partition_file.write('#nexus\n')
-# partition_file.write('begin sets;\n')
-
 data = os.listdir(trimal_files_path)
+
+# Create partition file
+partition_file = open(output_path + 'partition_file.nex', 'w+')
+partition_file.write('#nexus\n')
+partition_file.write('begin sets;\n')
+
+# Variables
 gene_names = []
 big_fasta_dict = dict()
 
+# Create dict with species as keys without values
 for species in species_file:
     big_fasta_dict[species.strip()] = ''
 
-charpartition_mine = '  charpartition mine = '
-
+# Get gene names
 for i in data:
     if ".fasta.gz" in i:
         gene_names.append(i.split('.')[0].split('_')[1])
 
 start = 1
-
 counter = 0
+charpartition_mine = '  charpartition mine = '
 
+# Concatenate sequence of all genes per species
 for gene_name in gene_names:
-    print(gene_name)
-    print(counter)
     counter += 1
     with gzip.open(trimal_files_path + 'trimal_' + gene_name + '.fasta.gz', 'rt') as trimal_of_gene:
         for line in trimal_of_gene:
@@ -42,26 +50,29 @@ for gene_name in gene_names:
                 current_sequence = big_fasta_dict[trimal_species]
                 new_sequence = current_sequence + line.strip()
                 big_fasta_dict[trimal_species] = new_sequence
-        # length_of_gene = len(big_fasta_dict[trimal_species])
-        # end = length_of_gene
-        # partition_file.write('  charset ' + gene_name + ' = ' + str(start) + '-' + str(end) + ';' + '\n')
-        # start = end + 1
 
-    # IQtree_output = open(IQtree_files_path + gene_name + '.iqtree')
-    # for line in IQtree_output:
-    #     if 'Best-fit model according to BIC:' in line:
-    #         best_model_BIC = line.split(': ')[1]
-    #         charpartition_mine += str(best_model_BIC.strip()) + ':' + gene_name + ', '
+        length_of_gene = len(big_fasta_dict[trimal_species])
+        end = length_of_gene
+        partition_file.write('  charset ' + gene_name + ' = ' + str(start) + '-' + str(end) + ';' + '\n')
+        start = end + 1
 
-# # Remove the last ', ' of charpartition
-# charpartition_mine = charpartition_mine[:-2]
-#
-# # Add ';' to charpartition
-# charpartition_mine += ';'
-#
-# partition_file.write(charpartition_mine + '\n')
-# partition_file.write('end;')
+    IQtree_output = open(IQtree_files_path + gene_name + '.iqtree')
+    for line in IQtree_output:
+        if 'Best-fit model according to BIC:' in line:
+            best_model_BIC = line.split(': ')[1]
+            charpartition_mine += str(best_model_BIC.strip()) + ':' + gene_name + ', '
 
+# Finish partition file
+# Remove the last ', ' of charpartition
+charpartition_mine = charpartition_mine[:-2]
+
+# Add ';' to charpartition
+charpartition_mine += ';'
+
+partition_file.write(charpartition_mine + '\n')
+partition_file.write('end;')
+
+# Save new fasta file
 for species in species_file:
     species = species.strip()
     header = '>' + species
