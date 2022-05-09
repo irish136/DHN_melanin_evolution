1d6928cc203444cdf379ce27cdd55c811e2f2ad7
irish136 <62709970+irish136@users.noreply.github.com>
2022-05-09 09:51:01 +0200

    Update 2_annotations_class_families_orders.py

--- s9_annotation_species_trees/2_annotations_class_families_orders.py
+++ s9_annotation_species_trees/2_annotations_class_families_orders.py
@@ -1,9 +1,16 @@
+# This script produces itol annotation files for visualizing the class, order, family and genus of fungi
+# Input: .txt file with the JGI taxonomy + .txt file with species names
+# Output: annotation file
+
+# Import packages
 import random
 
+# Open directories and files
 jgi_taxonomy = open('JGI_taxonomy_species_class_order_family_genus.txt').readlines()
 species_file = open('840_species_names.txt').readlines()
 
 def create_annotation_file(taxonomy):
+    # Check if argument is appropriate
     if taxonomy == 'class':
         index = 1
     elif taxonomy == 'order':
@@ -16,6 +23,7 @@ def create_annotation_file(taxonomy):
         print(taxonomy + ' cannot be processed, try "class", "order", "family" or "genus"')
         exit()
 
+    # Create colors per rang
     rang = []
     dict_rang_color = dict()
 
@@ -29,6 +37,7 @@ def create_annotation_file(taxonomy):
         hex_code = '#%02X%02X%02X' % (r(), r(), r())
         dict_rang_color[fungal_class.strip()] = hex_code
 
+    # Get legend info
     legend_shapes_string = 'LEGEND_SHAPES,'
     legend_labels_string = 'LEGEND_LABELS,'
     legend_colors_string = 'LEGEND_COLORS,'
@@ -43,7 +52,7 @@ def create_annotation_file(taxonomy):
     legend_colors_string = legend_colors_string[:-1]
     legend_labels_string = legend_labels_string[:-1]
 
-
+    # Create annotation file
     f = open("species_tree_itol_annotation_" + taxonomy + ".txt", 'w+')
     f.write("DATASET_COLORSTRIP\n")
     f.write("SEPARATOR COMMA\n")
@@ -63,4 +72,8 @@ def create_annotation_file(taxonomy):
                 color = dict_rang_color[species_rang.strip()]
                 f.write('CONTAINS==' + species + ',' + color + '\n')
 
+# Create annotation files
 create_annotation_file('class')
+create_annotation_file('order')
+create_annotation_file('family')
+create_annotation_file('genus')
